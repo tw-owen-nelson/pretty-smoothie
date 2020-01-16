5243bb813f21dba4c55295d71c254ad307d9256a
Owen Nelson <owen.nelson@thoughtworks.com>
2020-01-16 15:50:35 -0600

    Renames basic controller

--- src/main/java/com/team68/PrettySmoothie/controller/BasicController.java
+++ src/main/java/com/team68/PrettySmoothie/controller/BasicController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorldController {
+public class BasicController {
     @RequestMapping("/")
     public String index() {
         return "Hello smoothie lover!";
