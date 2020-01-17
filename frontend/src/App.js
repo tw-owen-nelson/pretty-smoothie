0544213ff6b344baa95351d33e2a23310911e247
Owen Nelson <owen.nelson@thoughtworks.com>
2020-01-17 17:38:07 -0600

    change logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import strawberry from './strawberry.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={strawberry} className="App-logo" alt="logo" />
+        <div className="App-intro">
+          <h2 className="mt-5">
+            Hello smoothie lover!
+          </h2>
+        </div>
       </header>
     </div>
   );
