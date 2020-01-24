df772758848530bad443150a2216b3f8318b08f0
Owen Nelson <owen.nelson@thoughtworks.com>
2020-01-24 10:53:24 -0600

    [#7][Owen] render components for predictor

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -2,19 +2,17 @@ import React from 'react';
 import strawberry from '../images/strawberry.png';
 import '../App.css';
 
-
-
 const Cute = () => {
-    return ( 
-      <div className="App-header">
-        <img src={strawberry} className="App-logo" alt="logo" />
-        <div className="pp-intro">
-          <h2 className="mt-5">
-            Hello smoothie lover!
-          </h2>
-        </div>
-       </div>
-    );
+  return (
+    <div className="App-header">
+      <img src={strawberry} className="App-logo" alt="logo" />
+      <div className="App-intro">
+        <h2 className="mt-5">
+          Hello smoothie lover!
+        </h2>
+      </div>
+     </div>
+  );
 }
 
 export default Cute;
--- frontend/src/components/Predictor.js
+++ frontend/src/components/Predictor.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
+import predictorImage from '../images/predictorImage.svg'
 
 class Predictor extends React.Component {
-    render() {
-        const predictorStyle = {
-            backgroundColor: '#f9faf7'
-        };
-
-        return (
-            <div style={predictorStyle}>
-                <Typography>Predictor View</Typography>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className='side-by-side'>
+        <div className='text-content'>
+          <h2>Hey there!</h2>
+          <p>
+            Tell us about your smoothie...<br/>
+            We{"'"}ll show you what it{"'"}ll look like.
+          </p>
+          <form>
+            <input type='radio' name='ingredient' value='1'/>
+            <input type='radio' name='ingredient' value='2'/>
+            <input type='radio' name='ingredient' value='3'/>
+            <br/>
+            <input type='submit' value='SHOW ME MY SMOOTHIE' className='smoothie-button'/>
+          </form>
+        </div>
+        <div className='doodle'>
+          <img src={predictorImage} alt='startup website doodle'  />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Predictor;
