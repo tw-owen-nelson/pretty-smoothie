6554e6abfcaf6c6c13ad07e19bd59df536c47567
Owen Nelson <owen.nelson@thoughtworks.com>
2020-01-29 10:58:41 -0600

    frontend loads icons from backend

--- frontend/src/components/FruitFactory.js
+++ frontend/src/components/FruitFactory.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import banana from '../images/banana.png'
-import blueberry from '../images/blueberry.png'
-import strawberry from '../images/strawberry.png'
-// https://stackoverflow.com/questions/45334874/load-images-based-on-dynamic-path-in-reactjs
+
 function FruitFactory(props) {
   switch(props.name) {
     case 'banana':
-      return <img src={banana} alt='banana icon'/>;
+      return <img src={'/media/banana.png'} alt='banana icon'/>;
     case 'blueberry':
-      return <img src={blueberry} alt='blueberry icon'/>;
+      return <img src={'/media/blueberry.png'} alt='blueberry icon'/>;
     case 'strawberry':
-      return <img src={strawberry} alt='strawberry icon'/>;
+      return <img src={'/media/strawberry.png'} alt='strawberry icon'/>;
     default:
       return <></>;
   }
--- src/main/java/com/team68/PrettySmoothie/WebConfiguration.java
+++ src/main/java/com/team68/PrettySmoothie/WebConfiguration.java
@@ -1,6 +1,7 @@
-package com.team68.PrettySmoothie.controller;
+package com.team68.PrettySmoothie;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,11 @@ public class WebConfiguration implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/{spring:(?!media)\\w+}")
                 .setViewName("forward:/");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/media/**")
+                .addResourceLocations("classpath:/public/");
     }
 }
