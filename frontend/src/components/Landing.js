12123c0e8b1a98f29b8e84a957d315a17d7141b1
Owen Nelson <owen.nelson@thoughtworks.com>
2020-02-14 11:07:04 -0600

    [Owen] fix css that I broke

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import strawberry from '../images/landingStrawberry.png';
+import { Typography } from '@material-ui/core';
 
 const Cute = () => {
   return (
     <div className="App-header">
       <img src={strawberry} className="App-logo" alt="strawberry" />
       <div className="App-intro">
-        <h2>
+        <Typography variant='h2'>
           Hello smoothie lover!
-        </h2>
+        </Typography>
       </div>
      </div>
   );
